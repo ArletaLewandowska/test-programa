8f09affa7ecc5f96e6ebcd999c961fa584f5ea7b
Arleta Lewandowska <arle.lewandowska@gmail.com>
2020-02-02 22:29:37 +0100

    aktualizacja testów logowania i rejestracji

--- cypress/integration/logowanie.spec.js
+++ cypress/integration/logowanie.spec.js
@@ -3,6 +3,8 @@
 context('Strona Logowania', () => {
     beforeEach(() => {
         cy.visit('http://qa-test.programa.pl/user/login')
+        // dodany czas 5s, ponieważ bpojawiał się błąd 429 - "Too Many Request" 
+        cy.wait(5000)
     })
 
     context('porawne logowanie', () => {
@@ -26,8 +28,9 @@ context('Strona Logowania', () => {
     })
 
     context('niepoprawne logowanie', () => {
-        
-        it('nie loguje się wprowadzenia bez danych', () => {
+
+        //brak danych
+        it('nie loguje się bez wprowadzenia danych', () => {
             cy.get('#user-email')
             cy.get('.btn.btn-primary')
                 .click()
@@ -38,7 +41,22 @@ context('Strona Logowania', () => {
             cy.get('span[data-error-for="user-login-email[password]"]').should('have.text', 'To pole jest wymagane')
         })
 
-        it('nie loguje się błednymi danymi', () => {
+        //błędne dane
+        it('nie loguje się błędnymi danymi', () => {
+            cy.get('#user-email')
+                .type('asdfghjklqwertyu').should('have.value', 'asdfghjklqwertyu')
+            cy.get('#user-password')
+                .type('asdfghjklqwertyu')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //niezarejestrowany email
+        it('nie loguje się danymi niezarejestrowanego użytkownika', () => {
             cy.get('#user-email')
                 .type('abcdefg@hij.com').should('have.value', 'abcdefg@hij.com')
             cy.get('#user-password')
@@ -51,6 +69,91 @@ context('Strona Logowania', () => {
             cy.get('div.alert p').should('have.text', 'Błędne dane logowania.')
         })
 
+        //email bez weryfikacji
+        it('nie loguje się danymi użytkownika zarejestrowanego, ale nie zweryfikowanego linkiem aktywacyjnym', () => {
+            cy.get('#user-email')
+                .type('aaabbbccc@aaa.bbb').should('have.value', 'aaabbbccc@aaa.bbb')
+            cy.get('#user-password')
+                .type('aaAAaa11')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('div.alert p').should('have.text', 'To konto nie zostało zweryfikowane. Odbierz wiadomość email, aby dokończyć rejestrację.')
+        })
+
+        //brak "@" w mailu
+        it('nie loguje zarejestrowanego użytkownika bez "@" w polu "Email"', () => {
+            cy.get('#user-email')
+                .type('yaq90292zzrgg.com').should('have.value', 'yaq90292zzrgg.com')
+            cy.get('#user-password')
+                .type('Aa1aaaaa')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak domeny w mailu
+        it('nie loguje zarejestrowanego użytkownika bez nazwy domeny w polu "Email"', () => {
+            cy.get('#user-email')
+                .type('yaq90292@').should('have.value', 'yaq90292@')
+            cy.get('#user-password')
+                .type('Aa1aaaaa')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak rozszerzenia domeny w mailu
+        it('nie loguje zarejestrowanego użytkownika bez rozszerzenia nazwy domeny w polu "Email"', () => {
+            cy.get('#user-email')
+                .type('yaq90292@zzrgg').should('have.value', 'yaq90292@zzrgg')
+            cy.get('#user-password')
+                .type('Aa1aaaaa')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak adresy internetowego w mailu
+        it('nie loguje zarejestrowanego użytkownika bez adresu internetowego w polu "Email"', () => {
+            cy.get('#user-email')
+                .type('yaq90292@.com').should('have.value', 'yaq90292@.com')
+            cy.get('#user-password')
+                .type('Aa1aaaaa')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak identyfikatora użytkownika w mailu
+        it('nie loguje zarejestrowanego użytkownika bez identyfikatora użytkownika w polu "Email"', () => {
+            cy.get('#user-email')
+                .type('@zzrgg.com').should('have.value', '@zzrgg.com')
+            cy.get('#user-password')
+                .type('Aa1aaaaa')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //błędne hasło
         it('nie loguje zarejestrowanego użytkownika z błednymi hasłem', () => {
             cy.get('#user-email')
                 .type('yaq90292@zzrgg.com').should('have.value', 'yaq90292@zzrgg.com')
@@ -62,6 +165,18 @@ context('Strona Logowania', () => {
             cy.url().should('not.equal', 'http://qa-test.programa.pl/')
             cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
             cy.get('div.alert p').should('have.text', 'Błędne dane logowania.')
+        })
+
+        //brak hasła
+        it('nie loguje zarejestrowanego użytkownika bez hasła', () => {
+            cy.get('#user-email')
+                .type('yaq90292@zzrgg.com').should('have.value', 'yaq90292@zzrgg.com')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/login')
+            cy.get('span[data-error-for="user-login-email[password]"]').should('have.text', 'To pole jest wymagane')
         })
 
     })
--- cypress/integration/rejestracja.spec.js
+++ cypress/integration/rejestracja.spec.js
@@ -6,6 +6,8 @@ const uuidv4 = require('uuid/v4');
 context('Strona Rejestracji', () => {
     beforeEach(() => {
         cy.visit('http://qa-test.programa.pl/user/register')
+        // dodany czas 3s, ponieważ pojawiał się błąd 429 - "Too Many Request" 
+        cy.wait(3000)
     })
 
     afterEach(() =>
@@ -35,6 +37,7 @@ context('Strona Rejestracji', () => {
 
     context('nieporawna rejestracja', () => {
 
+        //dane aktywnego użytkownika
         it('nie rejestruje się z danymi aktywnego użytkownika', () => {
             cy.get('#user-email')
                 .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')
@@ -51,7 +54,8 @@ context('Strona Rejestracji', () => {
             cy.get('div.alert h2').should('have.text', 'Ten adres email jest już zarejestrowany.')
         })
 
-        it('nie rejestruje się wprowadzenia bez danych', () => {
+        //brak danych
+        it('nie rejestruje się bez wprowadzenia danych', () => {
             cy.get('.btn.btn-primary')
                 .click()
 
@@ -62,7 +66,93 @@ context('Strona Rejestracji', () => {
             cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'To pole jest wymagane')
         })
 
-        it('nie rejestruje się wprowadzenia bez hasła', () => {
+        //brak "@"
+        it('nie rejestruje się bez wprowadzenia prawidlowego maila - brak znaku "@"', () => {
+            cy.get('#user-email')
+                .type('abcdefghijk.com').should('have.value', 'abcdefghijk.com')
+                cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('bbBB22bb')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('bbBB22bb')
+
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak domeny
+        it('nie rejestruje się bez wprowadzenia prawidlowego maila - brak nazwy domeny', () => {
+            cy.get('#user-email')
+                .type('abcdefg@').should('have.value', 'abcdefg@')
+                cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('bbBB22bb')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('bbBB22bb')
+
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak rozszerzenia domeny
+        it('nie rejestruje się bez wprowadzenia prawidlowego maila - brak rozszerzenia nazwy domeny', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk').should('have.value', 'abcdefg@hijk')
+                cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('bbBB22bb')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('bbBB22bb')
+
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak adresu internetowego
+        it('nie rejestruje się bez wprowadzenia prawidlowego maila - brak adresu internetowego domeny', () => {
+            cy.get('#user-email')
+                .type('abcdefg@.com').should('have.value', 'abcdefg@.com')
+                cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('bbBB22bb')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('bbBB22bb')
+
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak identyfikatora użytkownika
+        it('nie rejestruje się bez wprowadzenia prawidlowego maila - brak identyfikatora użytkownika', () => {
+            cy.get('#user-email')
+                .type('@hijk.com').should('have.value', '@hijk.com')
+                cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('bbBB22bb')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('bbBB22bb')
+
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[email]"]').should('have.text', 'Nieprawidłowy adres e-mail')
+        })
+
+        //brak hasła
+        it('nie rejestruje się bez wprowadzenia hasła', () => {
             cy.get('#user-email')
                 .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')
             cy.get('.btn.btn-primary')
@@ -74,11 +164,24 @@ context('Strona Rejestracji', () => {
             cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'To pole jest wymagane')
         })
 
-        it('nie rejestruje się z błędnym polem "Powtórz nowe hasło"', () => {
-            const randomemailpart = uuidv4();
+        //brak potwierdzenia hasła
+        it('nie rejestruje się bez wprowadzenia hasła w pole "Powtórz nowe hasło"', () => {
             cy.get('#user-email')
-                .type(randomemailpart+'@abc.com').should('have.value', randomemailpart+'@abc.com')
-            
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')
+            cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('bbBB22bb')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'To pole jest wymagane')
+        })
+
+        //błędne potwierdzenie hasła
+        it('nie rejestruje się z błędnym polem "Powtórz nowe hasło"', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')      
             cy.get('input[data-field-name="user-login-email[password][password]"]')
                 .type('bbBB22bb')
             cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
@@ -91,7 +194,100 @@ context('Strona Rejestracji', () => {
             cy.get('span[data-error-for="user-login-email[password]"]').should('have.text', 'Wartości nie sa takie same')
         })
 
+        //hasło za długie
+        //bez powtarzania hasła -  'have.text', 'To pole jest wymagane'
+        it('nie rejestruje się z hasłem niezgodnym z walidacją długości pola - powyżej 255 znaków', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')  
+            cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('TVLDteES6SJcwM3zUzJoKT36HujrE7RYt33mG8CdqMQnYhmkhu7tMzXVtFLyc4W6vpRCWUfL7wywdkfUeLMsLbNEgZCTtYpoNEraY6nw2msE58ZJkdAvNNzudBM62BTeGJP6fB3GnULRscZbPZ73e6WR8aozbFdhQtNwugCfAPwtrW4FnA9Z6vhD65CFbgpr9Cn4jqJkEFcv3FANFaec5VfzqjF2YvSyxD3gubZygCuHHvVN5AqzV6zLNEcdAucy')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('TVLDteES6SJcwM3zUzJoKT36HujrE7RYt33mG8CdqMQnYhmkhu7tMzXVtFLyc4W6vpRCWUfL7wywdkfUeLMsLbNEgZCTtYpoNEraY6nw2msE58ZJkdAvNNzudBM62BTeGJP6fB3GnULRscZbPZ73e6WR8aozbFdhQtNwugCfAPwtrW4FnA9Z6vhD65CFbgpr9Cn4jqJkEFcv3FANFaec5VfzqjF2YvSyxD3gubZygCuHHvVN5AqzV6zLNEcdAucy')
+            cy.get('.btn.btn-primary')
+                .click()
 
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[password][password]"]').should('have.text', 'Ilość znaków powinna zawierać się między 8 a 255')
+            cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'Ilość znaków powinna zawierać się między 8 a 255')
+         
+        })
+
+        //hasło za krótkie
+        //bez powtarzania hasła -  'have.text', 'To pole jest wymagane'
+        //błąd - nieprawidłowy komunikat walidacji - oczekiwany text pola span "Ilość znaków powinna zawierać się między 8 a 255" w polach "Hasło" i "Powtórz nowe hasło"
+        //wykonać test, który się nie wykona ze względu na prawidłową wartość 'have.text'
+        it('nie rejestruje się z hasłem niezgodnym z walidacją długości pola - poniżej 8 znaków', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')       
+            cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('aa')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('aa')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[password][password]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')
+            cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')
+        })
+
+        //hasło - brak wielkiej litery
+        //bez powtarzania hasła -  'have.text', 'To pole jest wymagane'
+        it('nie rejestruje się z hasłem niezgodnym z walidacją - brak wielkiej litery', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')
+            cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('aaaaaa11')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('aaaaaa11')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[password][password]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')
+            cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')             
+        })
+
+        //hasło - brak małej litery
+        //bez powtarzania hasła -  'have.text', 'To pole jest wymagane'
+        it('nie rejestruje się z hasłem niezgodnym z walidacją - brak małej litery', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')
+            cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('AAAAAA11')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('AAAAAA11')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[password][password]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')
+            cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')             
+        })
+
+        //hasło - brak znaku specjalnego lub cyfry
+        //bez powtarzania hasła -  'have.text', 'To pole jest wymagane'
+        //błąd - nieprawidłowy komunikat walidacji - oczekiwany text pola span "Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny lub cyfrę."
+        //wykonać test, który się nie wykona ze względu na prawidłową wartość 'have.text'
+        it('nie rejestruje się z hasłem niezgodnym z walidacją - brak cyfry lub znaku specjalnego', () => {
+            cy.get('#user-email')
+                .type('abcdefg@hijk.com').should('have.value', 'abcdefg@hijk.com')
+            cy.get('input[data-field-name="user-login-email[password][password]"]')
+                .type('aaaaaaAA')
+            cy.get('input[data-field-name="user-login-email[password][password_repeat]"]')
+                .type('aaaaaaAA')
+            cy.get('.btn.btn-primary')
+                .click()
+
+            cy.url().should('not.equal', 'http://qa-test.programa.pl/registerSuccess')
+            cy.url().should('equal', 'http://qa-test.programa.pl/user/register')
+            cy.get('span[data-error-for="user-login-email[password][password]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')
+            cy.get('span[data-error-for="user-login-email[password][password_repeat]"]').should('have.text', 'Hasło musi zawierać przynajmniej jedną wielką, jedną małą literę oraz jeden znak specjalny.')             
+        })
     })
     
 })
